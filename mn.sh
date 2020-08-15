a005a7d77fc009332a322656e4675db17020d173
Aniran <aniranjose@gmail.com>
2020-08-16 00:22:54 +0200

    Refactoring to have separated dir for tags instead of one file containing all tags.

--- mn.sh
+++ mn.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #set -x
-source ./script_ctl.src
-source ./fmt_font.src    
+APP_HOME=$(dirname $(readlink $0))
+
+source $APP_HOME/script_ctl.src
+source $APP_HOME/fmt_font.src    
 
 function fn_file_exists  () { [ -f "$1" ] || msg_quit "$1: not found."; }
 function get_param () {
@@ -14,14 +16,15 @@ function get_param () {
 BASH_V=$(bash_version)
 HOMEDIR=~
 CMD_NAME=$(basename ${0%.sh})
-CONFIG_FILE=$(pwd -P $0)/config
-GIT_LATEST_PULL=$(pwd -P $0)/.git_latest_pull
+CONFIG_FILE=$APP_HOME/config
+GIT_LATEST_PULL=$APP_HOME/.git_latest_pull
 DEFAULT_GIT_URL_MSG=INSERT_CLONE_URL
 
 function fn_git () { git -C $DATA_DIR $*; }
 function fn_check_latest_pull () {
+    [ ! -f "$GIT_LATEST_PULL" ] && echo "0" > $GIT_LATEST_PULL
     local secs_latest_pull=$(( $(date +"%s") - $(cat $GIT_LATEST_PULL) ))
-    [ "$secs_latest_pull" -gt "$(( $GIT_PULL_INTERVAL * 3600 ))" ] \
+    [ "$secs_latest_pull" -gt "$GIT_PULL_INTERVAL" ] \
     && fn_git pull \
     && echo $(date +"%s") > $GIT_LATEST_PULL
 }
@@ -29,7 +32,7 @@ function fn_check_latest_pull () {
 if [ -f "$CONFIG_FILE" ]; then
     DATA_DIR=$(get_param 'data_dir' $CONFIG_FILE)
     GIT_REPO=$(get_param 'git_repo' $CONFIG_FILE)
-    GIT_PULL_INTERVAL=$(get_param 'pull_hours_interval' $CONFIG_FILE)
+    GIT_PULL_INTERVAL=$(( $(get_param 'pull_hours_interval' $CONFIG_FILE) * 3600 ))
     
     if [ "$GIT_REPO" = "$DEFAULT_GIT_URL_MSG" ]; then
         msg_quit "Please specify your git_repo from $CONFIG_FILE"
@@ -40,13 +43,13 @@ if [ -f "$CONFIG_FILE" ]; then
         echo "Cloning $GIT_REPO to $DATA_DIR"
         [ "$(ls -A $DATA_DIR)" ] && msg_quit "$DATA_DIR is not empty, cant proceed."
         git -C $DATA_DIR clone $GIT_REPO $DATA_DIR && echo "done." 
-        echo "$(date +\"%s\")" > $GIT_LATEST_PULL 
+        echo $(date +"%s") > $GIT_LATEST_PULL 
         read -n1 -p "Press any key to continue..."
     fi
 else
     echo "Creating config file"
     echo -e "\
-# Where your notes tags and metadata will be saved.\n\
+# Storage for notes and tags.\n\
 data_dir $HOMEDIR/var/$CMD_NAME\n\
 \n# Github repo URL to backup your notes.\n\
 git_repo $DEFAULT_GIT_URL_MSG\n\
@@ -56,22 +59,24 @@ pull_hours_interval 24" > $CONFIG_FILE \
     exit 0
 fi
 
-METADATA=$DATA_DIR/metadata     ; [ -f "$METADATA" ] || echo "index 0" > $METADATA
-NOTES_DIR=$DATA_DIR/notes       ; mkdir -p $NOTES_DIR
-TAGS=$DATA_DIR/tags             ; touch $TAGS
+METADATA=$DATA_DIR/metadata   ; [ -f "$METADATA" ] || echo "index 0" > $METADATA
+NOTES_DIR=$DATA_DIR/notes     ; mkdir -p $NOTES_DIR
+TAGS_DIR=$DATA_DIR/tags       ; mkdir -p $TAGS_DIR
 TERM_COLS=$([ -n "$COLUMNS" ] && echo $COLUMNS || tput cols)
 TERM_ROWS=$([ -n "$LINES"   ] && echo $LINES   || tput lines)
 BLANK_LINE="$(printf %${TERM_COLS}s)"
 DEFAULT_MODE="0600"
 
-function ff_topic   () { echo $(fmt_font bold  color white  "$1"); }
-function ff_index   () { echo $(fmt_font color light_yellow "$1"); }
-function ff_content () { echo $(fmt_font color dark_gray    "$1"); }
-function ff_tags    () { echo $(fmt_font color cyan         "$1"); }
+function ff_topic    () { echo $(fmt_font bold  color white   "$1"); }
+function ff_index    () { echo $(fmt_font color light_yellow  "$1"); }
+function ff_content  () { echo $(fmt_font color dark_gray     "$1"); }
+function ff_tags     () { echo $(fmt_font color cyan          "$1"); }
+function ff_sel_tags () { echo $(fmt_font color light_cyan    "$1"); }
 
-function fn_preview_note () { head -c $TERM_COLS $NOTES_DIR/$1 2>/dev/null; }
+function fn_preview_note () { echo -e $(ff_content "$(head -c $TERM_COLS $NOTES_DIR/$1 2>/dev/null)"); }
+
 function fn_unique_tags  () { 
-    (for ii in $(cat $TAGS | cut -d ' ' -f 2- ); do echo $ii; done;) | sort --unique 
+    (for ii in $(cat $TAGS_DIR/*); do echo $ii; done;) | sort --unique 
 }
 function fn_unique_tags_inline () { fn_unique_tags | paste -s -; }
 
@@ -119,13 +124,13 @@ function new_note () {
 
     vim $new_file_path \
     && echo "Saving note $(ff_index $index): $(ff_tags $new_tags)" \
-    && echo "$index $new_tags" >> $TAGS \
+    && echo "$new_tags" > $TAGS_DIR/$index \
     && set_param index $METADATA $index \
     && fn_git add $new_file_path \
     && commit_push
 }
 
-function grep_tags () {
+function shell_grep_tags () {
     local unique_tags="$(fn_unique_tags) "
     if [ -z "$1" ]; then :
     else
@@ -137,30 +142,36 @@ function grep_tags () {
     echo "$unique_tags"
 }
 
-function grep_note () {
-    #set -x
+function grep_by_tags () {
     [ -z "$1" ] && msg_quit "Empty grep query"
     local whole_query="$*"
-    local lines_matching="$(cat $TAGS)"
-
-    for ii in $whole_query; do
-        [ -z "$lines_matching" ] && break
-        echo "\$ii=$ii"
-        echo "\$lines_matching=$lines_matching"; read -n1
-        lines_matching="$(echo \"$lines_matching\" | grep $ii)"
-    done
-
-    echo "\$lines_matching=$lines_matching"; read -n1
 
     local pattern="${1// /\\|}"
-    local tag_search_result=$(grep $pattern $TAGS | cut -d ' ' -f 1)
-
-    for ii in $tag_search_result; do
-        local tsr_files+="$NOTES_DIR/$ii "
-    done
+    local files_list="$(find $TAGS_DIR -type f)"
 
     for ii in "$whole_query"; do
-        files_list="$(grep $ii -l $files_list)" 
+        [ -z "$files_list" ] && return
+        local files_list="$(grep $ii -l $files_list)" 
+    done
+
+    for ii in $files_list; do
+        local index=$(basename $ii)
+        local remaining_tags=$(cat $ii | sed "s/$pattern//g")
+        echo -e $(ff_index $index)":"$(ff_sel_tags "$whole_query")" "$(ff_tags "$remaining_tags")
+        fn_preview_note $index
+        echo ""
+    done
+}
+
+function grep_note () {
+    [ -z "$1" ] && msg_quit "Empty grep query"
+    local whole_query="$*"
+
+    local pattern="${1// /\\|}"
+    local files_list="$(find $NOTES_DIR -type f)"
+
+    for ii in "$whole_query"; do
+        local files_list="$(grep $ii -l $files_list)" 
     done
 
     for ii in $files_list; do
@@ -171,25 +182,24 @@ function grep_note () {
 }
 
 function list_notes () {
-    [ -s "$TAGS" ] || msg_quit "$TAGS is empty."
+    [ "$(ls -1 $TAGS_DIR | wc -l)" = "0" ] && msg_quit "$TAGS_DIR is empty."
 
     local bkifs="$IFS"
     IFS=$'\n'
-    for ii in $(cat $TAGS); do
+    for ii in $(ls -1 $TAGS_DIR); do
         local index=${ii%% *}
-        local tags=${ii#* }
-        echo "$(ff_index $index): "$(ff_tags "$tags")
-        echo -e $(ff_content "$(fn_preview_note $index)") 
+        local tags=$(cat $TAGS_DIR/$ii)
+        echo "$(ff_index $ii): "$(ff_tags "$tags")
+        fn_preview_note $ii
     done
     IFS="$bkifs"
     echo ""
 }
 
-function validate_note_has_tags () { grep -q "^$1 " $TAGS || msg_quit "Tags not found for $id"; }
-
-function load_note_tags () { grep "^$1 " $TAGS | cut -d ' ' -f 2-; }
-
-function fn_remove_tags_from () { sed -i .tmp '/^'$1' /d' $TAGS; rm ${TAGS}.tmp; }
+function validate_note_has_tags () {
+    local tag=$TAGS_DIR/$1
+    [ -f "$tag" ] && [ -s "$tag" ] || msg_quit "Tags not found for $1"
+}
 
 function edit_note () {
     local id=$1
@@ -197,7 +207,7 @@ function edit_note () {
 
     if [ -f "$note_file_path" ]; then
         validate_note_has_tags $id 
-        local old_tags=$(load_note_tags $id); local new_tags=""
+        local old_tags=$(cat $NOTES_DIR/$id); local new_tags=""
         
         if [ "$BASH_V" -gt "3" ]; then
             while [ -z "$new_tags" ]; do read -e -i "$old_tags" -p "Tags: " new_tags; done
@@ -207,11 +217,7 @@ function edit_note () {
             [ -z "$new_tags" ] && new_tags="$old_tags"
         fi
 
-        if [ "$new_tags" = "$old_tags" ]; then :
-        else
-            fn_remove_tags_from $id
-            echo "$id $new_tags" >> $TAGS  # Add line with new tag set
-        fi
+        [ "$new_tags" = "$old_tags" ] || echo "$new_tags" > $TAGS_DIR/$id 
 
         vim $note_file_path && commit_push
     else
@@ -229,9 +235,7 @@ function cat_note () {
 function rm_note () {
     local id=$1
     [ -z "$id" ] && echo "id is empty" && list_notes && return
-    local note=$NOTES_DIR/$id
-    fn_remove_tags_from $id 
-    [ -f "$note" ] && rm $note
+    rm $NOTES_DIR/$id $TAGS_DIR/$id 2>/dev/null
     list_notes
 }
 
@@ -247,7 +251,7 @@ function mn_shell () {
         tput cup 0 0
         local LEN_CUR_STR=${#CUR_STR}
         echo -e "Welcome to $CMD_NAME shell. Type :q to quit, :h for instructions.\n"
-        local filtered_tags="$(grep_tags $CUR_STR | paste -s -)"
+        local filtered_tags="$(shell_grep_tags $CUR_STR | paste -s -)"
         local formatted_ft=$(ff_tags "${filtered_tags}")
         echo -e "${formatted_ft}${BLANK_LINE:0:$(( ${#BLANK_LINE} - ${#filtered_tags} ))}"
         [[ "$CUR_STR" == *" "* ]] && grep_note $CUR_STR
@@ -278,14 +282,15 @@ function mn_shell () {
 [ -z "$1" ] && mn_shell && exit 0
 
 case $1 in
-    *help)   print_usage                   ;;
-    shell)   shift; mn_shell               ;;
-    new)     shift; new_note $*            ;;
-    grep)    shift; grep_note $*           ;;
-    edit)    shift; edit_note "$@"         ;;
-    list)    shift; list_notes             ;;
-    rm)      shift; rm_note $1             ;;
-    show)    shift; cat_note $1            ;;
-    install) shift; fn_install             ;;
-    *)       msg_quit "Invalid option: $1" ;;
+    *help)   print_usage                     ;;
+    shell)   shift; mn_shell                 ;;
+    new)     shift; new_note $*              ;;
+    grep)    shift; grep_note $*             ;;
+    tgrep)   shift; grep_by_tags $*          ;;
+    edit)    shift; edit_note "$@"           ;;
+    list)    shift; list_notes               ;;
+    rm)      shift; rm_note $1               ;;
+    show)    shift; cat_note $1              ;;
+    install) shift; fn_install               ;;
+    *)       msg_quit "Invalid option: $1"   ;;
 esac
