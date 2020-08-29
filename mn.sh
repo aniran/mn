ccf84d2e82e8362314b58aafeb580191abadf367
Aniran <aniranjose@gmail.com>
2020-08-30 00:34:05 +0200

    Implementing password protected content.

--- mn.sh
+++ mn.sh
@@ -5,7 +5,8 @@ APP_HOME=$(dirname $([ -L "$0" ] && readlink $0 || ls $0))
 source $APP_HOME/script_ctl.src
 source $APP_HOME/fmt_font.src    
 
-function fn_file_exists  () { [ -f "$1" ] || msg_quit "$1: not found."; }
+function fn_file_exists () { [ -f "$1" ] || msg_quit "$1: not found."; }
+
 function get_param () {
     local param=$1; local file=$2
     fn_file_exists $file
@@ -13,16 +14,15 @@ function get_param () {
     [ -n "$loaded_param" ] && echo $loaded_param 
 }
 
-BASH_V=$(bash_version)
-HOMEDIR=~
-CMD_NAME=$(basename ${0%.sh})
-CMD_BASENAME=$(basename $0)
-CMD_REAL_PATH=$(pwd)/$(basename $0)
 CONFIG_FILE=$APP_HOME/config
 GIT_LATEST_PULL=$APP_HOME/.git_latest_pull
 DEFAULT_GIT_URL_MSG=INSERT_CLONE_URL
 GIT_MAJOR=$(cut -d . -f 1 <<<$(git --version | awk '{print $3}'))
 MD5=$(type -t md5sum &>/dev/null && echo md5sum || echo md5)
+
+for ii in $MD5 git openssl; do
+    type -t $ii &>/dev/null || msg_quit "Missing dependency: $ii"
+done
 
 function fn_git () { cd $DATA_DIR && git $* && cd - ; }
 
@@ -41,13 +41,16 @@ if [ -f "$CONFIG_FILE" ]; then
     
     if [ "$GIT_REPO" = "$DEFAULT_GIT_URL_MSG" ]; then
         msg_quit "Please specify your git_repo from $CONFIG_FILE"
-    elif git -C $DATA_DIR status &>/dev/null; then
+    elif [ -d "$DATA_DIR" ] && cd $DATA_DIR && git status &>/dev/null && cd - &>/dev/null; then
         fn_check_latest_pull
     else
-        mkdir -p $DATA_DIR
-        echo "Cloning $GIT_REPO to $DATA_DIR"
+        mkdir -p $DATA_DIR &>/dev/null
         [ "$(ls -A $DATA_DIR)" ] && msg_quit "$DATA_DIR is not empty, cant proceed."
-        git -C $DATA_DIR clone $GIT_REPO $DATA_DIR && echo "done." 
+        echo "Cloning $GIT_REPO to $DATA_DIR"
+        cd $DATA_DIR \
+        && git clone $GIT_REPO $DATA_DIR \
+        && cd - &>/dev/null \
+        && echo "done." 
         echo $(date +"%s") > $GIT_LATEST_PULL 
         read -n1 -p "Press any key to continue..."
     fi
@@ -64,13 +67,10 @@ pull_hours_interval 24" > $CONFIG_FILE \
     exit 0
 fi
 
-NOTES_DIR=$DATA_DIR/notes     ; mkdir -p $NOTES_DIR
-TAGS_DIR=$DATA_DIR/tags       ; mkdir -p $TAGS_DIR
+NOTES_DIR=$DATA_DIR/notes ; mkdir -p $NOTES_DIR 2>/dev/null
+TAGS_DIR=$DATA_DIR/tags   ; mkdir -p $DATA_DIR 2>/dev/null
 UNIQUE_TAGS_FILE=$DATA_DIR/unique_tags
 INDEX_TRUC_FILE=$DATA_DIR/index_truncate
-TERM_COLS=$([ -n "$COLUMNS" ] && echo $COLUMNS || tput cols)
-TERM_ROWS=$([ -n "$LINES"   ] && echo $LINES   || tput lines)
-BLANK_LINE="$(printf %${TERM_COLS}s)"
 DEFAULT_MODE="0600"
 
 function ff_topic    () { echo $(fmt_font bold  color white   "$1"); }
@@ -79,13 +79,36 @@ function ff_content  () { echo $(fmt_font color dark_gray     "$1"); }
 function ff_tags     () { echo $(fmt_font color cyan          "$1"); }
 function ff_sel_tags () { echo $(fmt_font color light_cyan    "$1"); }
 
-function fn_preview_note () { echo -e $(ff_content "$(head -c $TERM_COLS $NOTES_DIR/$1 2>/dev/null)"); }
+function fn_preview_file () { 
+    file_in=$1
+    [ -f "$file_in" ] || msg_quit "File not found: $file_in"
+
+    if [ "$(head -c 6 $file_in)" = "Salted" ]; then
+        echo -e $(ff_content "Encrypted")
+    else
+        echo -e $(ff_content "$(head -c $TERM_COLS $file_in 2>/dev/null)")
+    fi
+}
 
 function update_unique_tags () {
     cat $TAGS_DIR/* \
     | tr ' ' '\n' \
     | sort \
     | uniq > $UNIQUE_TAGS_FILE
+}
+
+function fn_encrypt_file () {
+    local note=$1
+    echo -n "Enter password: "
+    openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 80 -salt -in $note -out $note.$CMD_TIME -pass pass:$(read -s && echo $REPLY) \
+    && mv $note.$CMD_TIME $note
+}
+
+function fn_decrypt_file () {
+    local note=$1
+    [ "$(head -c 6 $note)" = "Salted" ] \
+    && echo -n "Enter password: " \
+    && openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 80 -salt -d -in $note -out $note.$CMD_TIME -pass pass:$(read -s && echo $REPLY)
 }
 
 function update_index_truncate () {
@@ -98,8 +121,7 @@ function update_index_truncate () {
 
 function commit_push () { 
     local ftz=$(date +"%F %T %Z")
-    local sss=$(date +"%s")
-    local commit_msg_file=/tmp/commit_msg_file.$sss
+    local commit_msg_file=/tmp/commit_msg_file.$CMD_TIME
 
     if [ "$(fn_git status --porcelain=v1 2>/dev/null | wc -l)" -gt 0 ]; then
         echo "$ftz - $CMD_NAME backup." > $commit_msg_file 
@@ -135,13 +157,14 @@ function set_param () {
 }
 
 function new_note () {
-    [ -f "$1" ] && local import_file=$1 && shift
+    local id=$1
+    grep -q '^:pass.*' <<<$id && local password=T && shift && id=$1
+    [ -f "$id" ] && local import_file=$id && shift
     local new_tags="$*"
-    local tmp_file=$(date +"%s").tmp
 
     while [ -z "$new_tags" ]; do read -p "Tags: " new_tags; done
 
-    local tmp_file_path=$NOTES_DIR/$tmp_file
+    local tmp_file_path=$NOTES_DIR/tmp.$CMD_TIME
     
     [ -n "$import_file" ] && install -m $DEFAULT_MODE $import_file $tmp_file_path 
 
@@ -152,9 +175,12 @@ function new_note () {
     && echo "Saving note $(ff_index $new_md5): $(ff_tags $new_tags)" \
     && echo "$new_tags" > $TAGS_DIR/$new_md5 \
     && update_unique_tags \
-    && update_index_truncate \
-    && fn_git add $new_file_path \
-    && commit_push
+    && update_index_truncate 
+    
+    [ -n "$password" ] && fn_encrypt_file $new_file_path
+
+    fn_git add $new_file_path 
+    commit_push
 }
 
 function grep_note () {
@@ -192,7 +218,7 @@ function grep_by_tags () {
         local index=$(basename $ii)
         local remaining_tags=$(cat $ii | sed "s/$pattern//g")
         echo -e $(ff_index $index)":"$(ff_sel_tags "$whole_query")" "$(ff_tags "$remaining_tags")
-        fn_preview_note $index
+        fn_preview_file $ii
         echo ""
     done
 }
@@ -202,21 +228,19 @@ function list_notes () {
     [ "$wc_tags_dir" = "0" ] && msg_quit "$TAGS_DIR is empty."
     local index_trunc=$(cat $INDEX_TRUC_FILE)
 
-#    local bkifs="$IFS"
-#    IFS=$'\n'
     for ii in $(ls -1 $NOTES_DIR); do
-        #local index=${ii%% *}; index_trunc=${index::$index_trunc}
         id_trunc=${ii::$index_trunc}
         local tags=$(cat $TAGS_DIR/$ii)
         echo "$(ff_index $id_trunc): "$(ff_tags "$tags")
-        fn_preview_note $ii
+        fn_preview_file $NOTES_DIR/$ii
     done
-#    IFS="$bkifs"
     echo ""
 }
 
 function edit_note () {
     local id=$1
+    grep -q '^:pass.*' <<<$id && local password=T && shift && id=$1
+
     local note_file_path=$(ls -1 $NOTES_DIR/${id}*)
     local tags_file_path=$(ls -1 $TAGS_DIR/${id}*)
     local note_old_md5=$(basename $note_file_path)
@@ -242,9 +266,12 @@ function edit_note () {
         vim $note_file_path \
         && local new_md5=$($MD5 $note_file_path | awk '{print $1}') \
         && mv $note_file_path $NOTES_DIR/$new_md5 2>/dev/null \
+        && note_file_path=$NOTES_DIR/$new_md5 \
         && mv $TAGS_DIR/$note_old_md5 $TAGS_DIR/$new_md5 \
-        && update_index_truncate \
-        && commit_push
+        && update_index_truncate 
+        
+        [ -n "$password" ] && fn_encrypt_file $note_file_path
+        commit_push
     else
         msg_quit "Note $(ff_index $id) not found."
     fi
@@ -254,7 +281,10 @@ function cat_note () {
     local id=$1
     [ -z "$id" ] && msg_quit "USAGE: $CMD_NAME show <ID>"
     local note=$(ls -1 $NOTES_DIR/$id*)
-    [ -f "$note" ] && cat $note
+    [ ! -f "$note" ] && msg_quit "File not found: $note"
+    fn_decrypt_file $note
+    cat $note
+    [ -f $note.$CMD_TIME ] && mv $note.$CMD_TIME $note
 }
 
 function rm_note () {
