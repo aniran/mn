7750fd1a01561f30bfb5a25102505617ec86970e
Aniran <aniranjose@gmail.com>
2020-09-14 02:07:01 +0200

    Removing encrypt.sh from versioned because we'll use as-is from 'fastsite' project

--- mn.sh
+++ mn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #set -x
-APP_HOME=$(dirname $([ -L "$0" ] && readlink $0 || ls $0))
+APP_HOME=$(dirname $([ -L "$0" ] && readlink -e $0 || ls $0))
 
 source $APP_HOME/script_ctl.src
 source $APP_HOME/fmt_font.src    
@@ -19,12 +19,44 @@ GIT_LATEST_PULL=$APP_HOME/.git_latest_pull
 DEFAULT_GIT_URL_MSG=INSERT_CLONE_URL
 GIT_MAJOR=$(cut -d . -f 1 <<<$(git --version | awk '{print $3}'))
 MD5=$(type -t md5sum &>/dev/null && echo md5sum || echo md5)
+FS_ENC_PATH=$CMD_REAL_DIR/submodules/fastsitephp/scripts/shell/bash/encrypt.sh
+FS_ENC_TAG=1.4.2
+CMD_ENCRYPT=$APP_HOME/encrypt.sh
 
 for ii in $MD5 git openssl; do
     type -t $ii &>/dev/null || msg_quit "Missing dependency: $ii"
 done
 
-function fn_git () { cd $DATA_DIR && git $* && cd - &>/dev/null; }
+function fn_validate_alnum () {
+    grep -q -E '^[[:alnum:]]+$' <<<$1 || msg_quit "Argument '$1' should contain alpha-numeric characters"
+}
+
+function fn_encryption () {
+    [ ! -f $CMD_ENCRYPT ] || [ ! -x $CMD_ENCRYPT ] && msg_quit "Fail checking $CMD_ENCRYPT, please try running 'mn install'"
+    
+    local enc_command=$1
+    local     file_in=$2
+
+    [ ! -f $file_in ] && msg_quit "Error opening $file_in"
+
+    if grep -q -E '^[de]$' <<<$enc_command; then
+        $CMD_ENCRYPT -${enc_command} -i $file_in -p $(read -s -p "Password: " && echo $REPLY) 1>/dev/null
+        echo -e "\n"
+    else
+        msg_quit "Encryption command not found: '$enc_command'" 
+    fi
+}
+
+function fn_git () { 
+    if [ -d $1 ]; then
+        local operational_dir=$1
+        shift
+    else
+        local operational_dir=$DATA_DIR
+    fi
+
+    cd $operational_dir && git $* && cd - &>/dev/null
+}
 
 function fn_check_latest_pull () {
     [ ! -f "$GIT_LATEST_PULL" ] && echo "0" > $GIT_LATEST_PULL
@@ -69,23 +101,42 @@ fi
 
 NOTES_DIR=$DATA_DIR/notes ; mkdir -p $NOTES_DIR 2>/dev/null
 TAGS_DIR=$DATA_DIR/tags   ; mkdir -p $DATA_DIR 2>/dev/null
+CMD_TIME=$DATA_DIR/$CMD_TIME
 UNIQUE_TAGS_FILE=$DATA_DIR/unique_tags
 INDEX_TRUC_FILE=$DATA_DIR/index_truncate
-DEFAULT_MODE="0600"
+MODE_USR_RW="0600"
+MODE_USR_RWX="0700"
 
 function ff_topic    () { echo $(fmt_font bold  color white   "$1"); }
 function ff_index    () { echo $(fmt_font color light_yellow  "$1"); }
 function ff_content  () { echo $(fmt_font color dark_gray    "$1"); }
+function ff_normal   () { echo $(fmt_font color white        "$1"); }
 function ff_tags     () { echo $(fmt_font color cyan          "$1"); }
 function ff_sel_tags () { echo $(fmt_font color light_cyan    "$1"); }
 function ff_protectd () { echo $(fmt_font bgcolor dark_gray color black "$1"); }
 
+function fn_get_filepath_id () {
+    local id=$1
+    local file_dir=$2
+
+    [ -z "$id" ] && msg_quit "<ID> argument cannot be empty."
+    [ ! -d "$file_dir" ] && msg_quit "Directory path not found: $file_dir"
+
+    local entry_file=$(ls $file_dir/$id*)
+
+    [ $(wc -l <<<$entry_file) -gt 1 ] && msg_quit "More than 1 file found for id=$id"
+    [ ! -f $entry_file ]              && msg_quit "File not found: $entry_file" 
+
+    echo $entry_file
+} 
+
 function fn_preview_file () { 
     file_in=$1
-    [ -f "$file_in" ] || msg_quit "File not found: $file_in"
 
-    if [ "$(head -c 6 $file_in)" = "Salted" ]; then
-        echo -e $(ff_protectd "Encrypted")
+    [ ! -f "$file_in" ] && msg_quit "File not found: $file_in"
+
+    if [ $(cut -d '.' -f 2 <<<$file_in) = "enc" ]; then
+        echo $(ff_protectd "Encrypted")
     else
         echo -e $(ff_content "$(head -c $TERM_COLS $file_in 2>/dev/null)")
     fi
@@ -98,41 +149,29 @@ function update_unique_tags () {
     | uniq > $UNIQUE_TAGS_FILE
 }
 
-function fn_encrypt_file () {
-    local note=$1
-    echo -n "Enter password: "
-    openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 80 -salt -in $note -out $note.enc -pass pass:$(read -s && echo $REPLY) \
-        && mv $note $note.dec \
-        && mv $note.enc $note
-}
-
-function fn_decrypt_file () {
-    local note=$1
-    [ "$(head -c 6 $note)" = "Salted" ] \
-        && echo -n "Enter password: " \
-        && openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 80 -salt -d -in $note -out $note.dec -pass pass:$(read -s && echo $REPLY) \
-        && mv $note $note.enc \
-        && mv $note.dec $note
-}
-
 function update_index_truncate () {
     local len=1
-    while [ $((for ii in $(ls -1 $NOTES_DIR); do echo ${ii::$len}; done) | sort | uniq -d | wc -l) -gt 0 ]; do
+
+    function dup_notes_entries () {
+        for ii in $(ls -1 $NOTES_DIR); do echo ${ii::$1}; done
+    }
+
+    while [ $( dup_notes_entries $len | sort | uniq -d | wc -l) -gt 0 ]; do
         len=$(( len + 1 ))
     done
+
     echo $len > $INDEX_TRUC_FILE
 }
 
 function commit_push () { 
     local ftz=$(date +"%F %T %Z")
-    local commit_msg_file=/tmp/commit_msg_file.$CMD_TIME
 
     if [ "$(fn_git status --porcelain=v1 2>/dev/null | wc -l)" -gt 0 ]; then
-        echo "$ftz - $CMD_NAME backup." > $commit_msg_file 
+        echo "$ftz - $CMD_NAME backup." > $CMD_TIME
         fn_git add .
-        fn_git commit -a -F $commit_msg_file 
+        fn_git commit -a -F $CMD_TIME
         fn_git push 
-        rm $commit_msg_file 
+        rm $CMD_TIME
     fi
 }
 
@@ -162,28 +201,52 @@ function set_param () {
 }
 
 function new_note () {
-    local id=$1
-    grep -q '^:pass.*' <<<$id && local password=T && shift && id=$1
-    [ -f "$id" ] && local import_file=$id && shift
+    local arg1=$1
+
+    while [ "${arg1::1}" = ":" ]; do
+        case $arg1 in
+            :enc*) local mode_encrypt=T ;;
+            :imp*) local mode_import=T  ;;
+        esac
+        shift 
+        arg1=$1
+    done
+    
+    if [ "$mode_import" ]; then
+        local import_file=$arg1 
+        [ ! -f "$import_file" ] && msg_quit "Error while reading file '$import_file'"
+        shift
+    fi
+
     local new_tags="$*"
 
-    while [ -z "$new_tags" ]; do read -p "Tags: " new_tags; done
+    [ -z "$new_tags" ] && echo "Allowed characters: Uppercase and lowercase letters, numbers, dash '-', underscore '_', dot '.'"
 
-    local tmp_file_path=$NOTES_DIR/tmp.$CMD_TIME
-    
-    [ -n "$import_file" ] && install -m $DEFAULT_MODE $import_file $tmp_file_path 
+    while [ -z "$new_tags" ]; do 
+        read -p "Tags: " new_tags
+        new_tags=$(tr -dc '[._ [:alnum:]-]' <<<$new_tags)
+    done
 
-    vim $tmp_file_path \
-    && local new_md5=$($MD5 $tmp_file_path | awk '{print $1}') \
+    [ "$mode_import" ] && install -m $MODE_USR_RW $import_file $CMD_TIME 
+
+    vim $CMD_TIME 
+
+    local new_md5=$($MD5 $CMD_TIME | awk '{print $1}') \
     && local new_file_path=$NOTES_DIR/$new_md5 \
-    && mv $tmp_file_path $new_file_path \
-    && echo "Saving note $(ff_index $new_md5): $(ff_tags $new_tags)" \
+    && mv $CMD_TIME $new_file_path 
+
+    if [ "$mode_encrypt" ]; then
+        fn_encryption e $new_file_path \
+        && rm $new_file_path \
+        && new_file_path=$new_file_path.enc \
+        && new_md5=$new_md5.enc
+    fi
+
+    echo "Saving note $(ff_index $new_md5): $(ff_tags $new_tags)" \
     && echo "$new_tags" > $TAGS_DIR/$new_md5 \
     && update_unique_tags \
     && update_index_truncate 
     
-    [ "$password" ] && fn_encrypt_file $new_file_path && rm $new_file_path.dec
-
     commit_push
 }
 
@@ -229,8 +292,12 @@ function grep_by_tags () {
 
 function list_notes () {
     local wc_tags_dir=$(ls -1 $TAGS_DIR | wc -l)
+
     [ "$wc_tags_dir" = "0" ] && msg_quit "$TAGS_DIR is empty."
+
     local index_trunc=$(cat $INDEX_TRUC_FILE)
+
+    echo ""
 
     for ii in $(ls -1 $NOTES_DIR); do
         id_trunc=${ii::$index_trunc}
@@ -242,103 +309,143 @@ function list_notes () {
 }
 
 function edit_note () {
-    #set -x
     local id=$1
+
     while [ "${id::1}" = ":" ]; do
         case $id in
-            :enc*) local encrypt=T     ;;
-            :dec*) local decrypt=T     ;;
-            :tag*) local change_tags=T ;;
+            :enc*) local enable_encrypt=T  ; unset disable_encrypt ;;
+            :dec*) local disable_encrypt=T ; unset enable_encrypt  ;;
+            :tag*) local change_tags=T                             ;;
+            *) msg_quit "Mode not found: '$id'"                    ;;
         esac
         shift 
         id=$1
     done
 
-    local note_file_path=$(ls -1 $NOTES_DIR/${id}*)
-    local tags_file_path=$(ls -1 $TAGS_DIR/${id}*)
-    local note_old_md5=$(basename $note_file_path)
+    fn_validate_alnum $id
 
-    if [ -f "$note_file_path" ]; then
-        [ -f "$tags_file_path" ] && [ -s "$tags_file_path" ] || msg_quit "Tags not found for $id"
-        local old_tags=$(cat $tags_file_path)
-        local new_tags=""
-        
-        if [ "$change_tags" ]; then
-            if [ "$BASH_V" -gt "3" ]; then
-                while [ -z "$new_tags" ]; do read -e -i "$old_tags" -p "Tags: " new_tags; done
-            else
-                echo -e "Current tags: "$(ff_tags "$old_tags")
-                read -p "Type new tags or [ENTER] to keep the current: " new_tags
-                [ -z "$new_tags" ] && new_tags="$old_tags"
-            fi
+    fn_get_filepath_id $id $NOTES_DIR 1>$CMD_TIME && read note_file_path < $CMD_TIME 
+    fn_get_filepath_id $id $TAGS_DIR  1>$CMD_TIME && read tags_file_path < $CMD_TIME
+
+    rm $CMD_TIME
+
+    local old_note_md5=$(basename $note_file_path)
+    local old_tags=$(cat $tags_file_path)
+    local new_tags=""
+    
+    if [ "$change_tags" ]; then
+        if [ "$BASH_V" -gt "3" ]; then
+            while [ -z "$new_tags" ]; do read -e -i "$old_tags" -p "Tags: " new_tags; done
+        else
+            echo -e "Current tags: "$(ff_tags "$old_tags")
+            read -p "Type new tags or [ENTER] to keep the current: " new_tags
+            [ -z "$new_tags" ] && new_tags="$old_tags"
         fi
-
         if [ ! "$new_tags" = "$old_tags" ]; then
             echo "$new_tags" > $tags_file_path
             update_unique_tags
         fi
-
-        fn_decrypt_file $note_file_path
-
-        vim $note_file_path \
-        && local new_md5=$($MD5 $note_file_path | awk '{print $1}') \
-        && new_note_file_path=$NOTES_DIR/$new_md5 \
-        && mv $note_file_path $new_note_file_path 2>/dev/null \
-        && mv $TAGS_DIR/$note_old_md5 $TAGS_DIR/$new_md5 \
-        && update_index_truncate 
-        
-        # If user wants to remove password protection
-        [ "$decrypt" ] && rm $note_file_path.enc
-
-        # If user wants to keep or switch password protection on
-        if [ "$encrypt" ] || [ -f $note_file_path.enc ]; then
-            echo "Encrypting $new_note_file_path"
-            fn_encrypt_file $new_note_file_path && rm $new_note_file_path.dec
-        fi
-
-        rm $note_file_path.enc 2>/dev/null        # Cleanup 
-        commit_push
-    else
-        msg_quit "Note $(ff_index $id) not found."
     fi
+
+    if grep -q -E '\.enc$' <<<$note_file_path; then
+        fn_encryption d $note_file_path        \
+        && rm $note_file_path                   \
+        && note_file_path=${note_file_path%.enc} \
+        && local file_was_encrypted=".enc"       || msg_quit "Error while decrypting $note_file_path"
+    fi
+
+    vim $note_file_path || msg_quit "Error while editing $note_file_path"
+
+    [ "$disable_encrypt" ] && unset file_was_encrypted
+
+    if [ "$file_was_encrypted" ] || [ "$enable_encrypt" ]; then
+        fn_encryption e $note_file_path \
+        && mv ${note_file_path}.enc $note_file_path \
+        && local file_was_encrypted=".enc" 
+    fi
+
+    local new_md5=$($MD5 $note_file_path | awk '{print $1}') 
+
+    mv $note_file_path $NOTES_DIR/${new_md5}${file_was_encrypted} 2>/dev/null
+    mv $tags_file_path $TAGS_DIR/${new_md5}${file_was_encrypted} 2>/dev/null
+
+    update_index_truncate 
+
+    #commit_push
+    #list_notes
 }
 
 function cat_note () {
     local id=$1
+
     [ -z "$id" ] && msg_quit "USAGE: $CMD_NAME show <ID>"
-    local note=$(ls -1 $NOTES_DIR/$id*)
-    [ ! -f "$note" ] && msg_quit "File not found: $note"
-    fn_decrypt_file $note
-    cat $note
-    [ -f $note.enc ] && mv $note.enc $note
+
+    fn_get_filepath_id $id $NOTES_DIR 1>$CMD_TIME
+    local note_file=$(cat $CMD_TIME) && rm $CMD_TIME
+
+    grep -q -E '.enc' <<<$note_file \
+    && fn_encryption d $note_file    \
+    && cat ${note_file%.enc}          \
+    && rm ${note_file%.enc}           || cat $note_file
 }
 
 function rm_note () {
     local id=$1
-    [ -z "$id" ] && echo "id is empty" && list_notes && return
-    rm $NOTES_DIR/$id $TAGS_DIR/$id 2>/dev/null \
-    && update_unique_tags \
-    && update_index_truncate \
-    && commit_push \
-    list_notes
+    [ -z "$id" ] && msg_quit "USAGE: $CMD_NAME rm <ID>"
+
+    fn_get_filepath_id $id $NOTES_DIR 1>$CMD_TIME
+    local note_file=$(cat $CMD_TIME)
+
+    fn_get_filepath_id $id $TAGS_DIR 1>$CMD_TIME
+    local tag_file=$(cat $CMD_TIME)
+
+    rm $CMD_TIME
+
+    echo -e "Files about to be deleted:\n\n$note_file\n$tag_file\n"
+
+    if read -p "Do you want to proceed ? [y/n] " && grep -q -E '^[Yy]([Ee][Ss])?$' <<<$REPLY; then
+        rm -f $note_file $tag_file \
+        && update_unique_tags \
+        && update_index_truncate \
+        && commit_push \
+        && echo "" \
+        && list_notes \
+        && echo "Record $(ff_index $id) was deleted."
+    fi
 }
 
 function fn_install () {
-    local complete_src=${CMD_NAME}.src
-    [ -d ~/init_source ] && ln -s $(pwd -P $0)"/$complete_src" ~/init_source/$complete_src 2>/dev/null
-    [ -d ~/bin ]         && ln -s $CMD_REAL_PATH ~/bin/$CMD_BASENAME 2>/dev/null
+    local complete_src=$CMD_REAL_DIR/complete_${CMD_NAME}.src
+    local complete_src_basename=$(basename $complete_src)
+
+    [ -d ~/init_source ] \
+    && [ -f $complete_src ] \
+    && ln -f -s $complete_src ~/init_source/$complete_src_basename
+
+    [ -d ~/bin ] \
+    && ln -f -s $CMD_REAL_PATH ~/bin/$CMD_BASENAME 
+
+    local FS_ENC_DIR=$(dirname $FS_ENC_PATH)
+
+    if [ -d $FS_ENC_DIR ]; then
+        fn_git $FS_ENC_DIR submodule init
+        fn_git $FS_ENC_DIR submodule update
+        fn_git $FS_ENC_DIR checkout $FS_ENC_TAG
+        install -m $MODE_USR_RWX $FS_ENC_PATH $CMD_REAL_DIR
+    else
+        msg_quit "Error checking out $FS_ENC_DIR"
+    fi
 }
 
 function comptag () {
     [ -z "$1" ] && cat $UNIQUE_TAGS_FILE | paste -s - && return
     local intersect="$*"
-    local pattern=$(sed -E 's/([[:alnum:]]*)/\\<\1\\>/g' <<<$intersect | sed 's/ /\\|/g')
+    local pattern=$(sed -E 's/([_[:alnum:].-]+)/\\<\1\\>/g' <<<$intersect | sed 's/ /\\|/g')
     echo "pattern=$pattern"
     grep -v $pattern $UNIQUE_TAGS_FILE | paste -s -
 }
 
 case $1 in
-    *help)   print_usage              ;;
     list)    list_notes               ;;
     install) fn_install               ;;
     save)    commit_push              ;;
@@ -352,4 +459,4 @@ case $1 in
     show)    shift; cat_note $1       ;;
     *)       print_usage              ;;
 esac
-#15
+#14
